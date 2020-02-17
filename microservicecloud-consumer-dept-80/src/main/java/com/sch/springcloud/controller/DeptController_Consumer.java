cfec0c3a9f373a546bb72f915081ad6c12f3d69b
shaochanghui <381493424@qq.com>
2020-02-17 10:05:56 +0800

    edit by branch hotfix

--- microservicecloud-consumer-dept-80/src/main/java/com/sch/springcloud/controller/DeptController_Consumer.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/sch/springcloud/controller/DeptController_Consumer.java
@@ -33,4 +33,9 @@ public class DeptController_Consumer {
 	public List<Dept> list() {
 		return restTemplate.getForObject(REST_URL_PREFIX+"/dept/list", List.class);
 	}
+	
+	public void test() {
+		System.out.println("test");
+	}
+
 }
